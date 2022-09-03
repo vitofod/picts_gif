55618dcfa16c310e39a15ce058ac92795a80a00e
vitofod <vitofode87@gmail.com>
2022-09-03 16:30:48 +0200

    Add setup.py, not complete yet.

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+
+from setuptools import setup, find_packages
+setup( name='',
+       version='',
+       author='  ',
+       author_email=' ',
+       packages=find_packages(),
+       package_dir={'': ''},
+       include_package_data=True,
+       license='MIT')
