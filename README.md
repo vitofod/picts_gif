b7adf73a25b4774aa65853ff08e04aa5cae0f316
vitofod <vitofode87@gmail.com>
2022-09-07 18:02:20 +0200

    adding installation procedure by setup.py file; include info in README.md

--- README.md
+++ README.md
@@ -3,8 +3,11 @@
 ## Installation
 Create anaconda virtual environment with:
 `conda env create --file environment.yml`
+Install the library 'picts_gif' with:
+`pip install .`
 ## Testing
-
+Test the library with:
+`pytest tests -v`
 ## Usage
 
 ## Tutorial
--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python
 
 from setuptools import setup, find_packages
-setup( name='',
+setup( name='picts_gif',
        version='',
        author='  ',
        author_email=' ',
        packages=find_packages(),
-       package_dir={'': ''},
+       package_dir={'picts_gif': 'picts_gif'},
        include_package_data=True,
        license='MIT')
