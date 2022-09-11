baeb60828bd7bfab6452a2b152cd5fde3556e415
vitofod <vitofode87@gmail.com>
2022-09-11 09:47:30 +0200

    Add some method in class tdms_input_handler.py and relative test

--- picts_gif/tdms_input_handler.py
+++ picts_gif/tdms_input_handler.py
@@ -42,7 +42,7 @@ class TdmsInputHandler:
         df.columns.name = 'Temperature (K)'
 
  
-        # Time values
+        # set_timetrack set the zero of the transient when led turn off
         if set_timetrack:
             df.index = tdms_file[data_group_name].channels()[0].time_track() # Take index from the first channel, it should be the same for all channels (i.e.) temperatures
         #df.index = df.index.values.round(3)
@@ -65,6 +65,8 @@ class TdmsInputHandler:
         - amplifier_gain:
             the gain value set on the current amplificator 
         '''
+        #check amplifier_gain > 0
+        if amplifier_gain < 0:  raise ValueError('In set_amplifier_gain: Not appropiate value.')
         # Current values
         df=df/amplifier_gain                # Convert to current
         return df  
@@ -83,9 +85,53 @@ class TdmsInputHandler:
             Useful for faulty dataset where there are some columns with more data than others
         -   drop: 
             list of 2 integers indicating the initial and final columns to be dropped from the dataframe as soon as it is imported. 
-            Usually used to remove the first or last columns which may contain corrupted data. If drop=[0,5] drop the first 2 columns\n
+            Usually used to remove the first or last columns which may contain corrupted data. If drop=[0,5] drop the first 4 columns\n
         '''
         if dropna: df=df.dropna()
         if drop is not None: df = df.drop(axis=1, columns = df.columns[drop[0]:drop[1]]) 
         return df
+
+    def set_zero_at_trigger(df, temperature):
+        '''
+        This method set the zero of the reference frame when led off.
+        ...............................................................
+
+        Input
+            - df: 
+                dataframe
+            - temperature: 
+                the float index temperature where take trigger values
+        Output
+            - df: 
+                dataframe
+        '''
+        if temperature < 0   raise ValueError('In set_zero_at_trigger: Not appropiate value.')
+        df.index -= df[temperature].diff().idxmin()
+        return df 
+
+    def normalized_transient(df, i_dark_range, i_light_range):
+        '''
+        This method normalized the current transient.
+        ................................................................
+
+        Input:
+            - df:
+                dataframe
+            - i_dark_range: 
+                a list of two float indicating the time interval in the current transient where to average the value of the dark current.
+            - i_light_range:
+                a list of two float indicating the time interval in the current transient where to average the value of the light current
+        Output
+            tr_norm:
+                dataframe
+        '''
+        if i_dark_range[0] > i_dark_range[1]:  raise ValueError('In normalized_transient: i_dark_range problem.')
+        if i_light_range[0] < i_dark_range[1]:  raise ValueError('In normalized_transient: i_light_range problem.')
+        i_light = df.loc[i_light_range[0]:i_light_range[1]].mean()
+        i_dark = df.loc[i_dark_range[0]:i_dark_range[1]].mean()
+        tr_norm = (df-i_inf)/(i_0-i_inf)
+        return tr_norm
+
+    
+
 
--- tests/test_tdms_input_handler.py
+++ tests/test_tdms_input_handler.py
@@ -38,3 +38,29 @@ class TestTdmsInputHandler:
         # with pytest.raises(Exception):
         TdmsInputHandler.read_transients(test_file_path, 'Measured Data', set_timetrack=True)
     
+
+    #Verifico che il metodo riceva un dataframe e un intero
+    def test_set_amplifier_gain_invalid_format(self):
+        #è un dataframe?
+        assert isinstance(df, pd.DataFrame)
+        #è un numero intero positivo?
+        assert type(amplifier_gain) == int, "Incorrect input, not an integer"
+    
+
+     def test_optimize_dataframe(self):
+        #è un dataframe?
+        assert isinstance(df, pd.DataFrame)
+        #dropna è un boolean?
+        assert type(dropna) == bool
+        #drop è una list di integer?
+
+    #Prima di essere settato, gli istanti temporali sono tutti positivi. Se si setta lo zero
+    # del tempo alla caduta del transiente, alla sinistra i dati saranno negativi    
+    def test_set_zero_at_trigger_go_well(self):
+        assert df.index[0] < 0
+
+    #il transiente normalizzato ha valori di corrente di luce compresi attorno 1, non normalizzato ~ 1e-9
+    def test_normalized_transient(self):
+        i_light = df.loc[i_light_range[0]:i_light_range[1]].mean()
+        i_light_norm = tr_norm.loc[i_light_range[0]:i_light_range[1]].mean()
+        assert i_light_norm > i_light
